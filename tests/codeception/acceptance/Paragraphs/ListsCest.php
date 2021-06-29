782410f5f827c3f1fda808de535b80a47e49cec3
Ian Monroe <ian@ianmonroe.com>
2021-06-29 12:38:57 -0600

    one more field, another test fixup

--- tests/codeception/acceptance/Paragraphs/ListsCest.php
+++ tests/codeception/acceptance/Paragraphs/ListsCest.php
@@ -28,7 +28,8 @@ class ListsCest {
       'display_id' => 'vertical_teaser_term',
       'items_to_display' => 100,
     ]);
-    drupal_flush_all_caches();
+    $I->wait(3);
+    //drupal_flush_all_caches();
     $I->amOnPage($node->toUrl()->toString());
     $I->canSee('Headliner');
     $I->canSee('Lorem Ipsum');
