1ddfc119d5d30f544265a848dd2ba5a09c9a2c2b
Marc Young <mdyoung3@stanford.edu>
2021-09-29 20:30:38 -0600

    Fixup

--- tests/codeception/acceptance/Content/SpotlightCest.php
+++ tests/codeception/acceptance/Content/SpotlightCest.php
@@ -27,8 +27,7 @@ class SpotlightCest
 
     $I->amOnPage("/spotlight");
     $I->see("Spotlights");
-    $I->seeElement('h1.heading-h1');
-//    $I->canSeeNumberOfElements(".soe-spotlight--cards .su-card", 2);
+    $I->canSeeNumberOfElements(".soe-spotlight--cards .su-card", 3);
 //    $I->seeElement('.su-card__contents');
 //    $I->seeElement(['css' => '.soe-link__button .button']);
 //    $I->click(["css" =>".soe-spotlight--cards--banner a.su-link"]);
