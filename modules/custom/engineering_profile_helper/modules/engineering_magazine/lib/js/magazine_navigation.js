018e376f5b4ad2453b2719d575bc402abea50229
Ian Monroe <ian@ianmonroe.com>
2021-09-30 14:21:07 -0600

    fixup removed debuggin

--- modules/custom/engineering_profile_helper/modules/engineering_magazine/lib/js/magazine_navigation.js
+++ modules/custom/engineering_profile_helper/modules/engineering_magazine/lib/js/magazine_navigation.js
@@ -8,12 +8,8 @@
             toggleButton.click(function () {
 
                 $('.magazine-landing-nav__topics-panel').slideToggle();
-                console.log('preparing to check');
-                console.log('prop = ' + toggleButton.attr("aria-expanded"));
                 if (toggleButton.attr('aria-expanded') == 'false') {
-                    console.log('got in');
                     toggleButton.attr('aria-expanded', 'true');
-                    console.log("after change: " + toggleButton.attr('aria-expanded'));
                     toggleButton.removeClass('soe-magazine__navigation-rotate-down');
                     toggleButton.addClass('soe-magazine__navigation-rotate-up');
                 } else {
