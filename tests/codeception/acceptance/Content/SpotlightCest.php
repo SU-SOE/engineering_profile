f00f093ee8248a49c09061c382bb5f5cb6fbc81f
Marc Young <mdyoung3@stanford.edu>
2021-09-29 18:09:12 -0600

    SOED9-139 - adding tests from new cut branch.

--- tests/codeception/acceptance/Content/SpotlightCest.php
+++ tests/codeception/acceptance/Content/SpotlightCest.php
@@ -2,9 +2,9 @@
 
 class SpotlightCest
 {
-/*    public function _before(AcceptanceTester $I)
-    {
-    }*/
+  /*    public function _before(AcceptanceTester $I)
+      {
+      }*/
 
   /**
    * Test that the view pages elements exists.
@@ -16,13 +16,47 @@ class SpotlightCest
     $I->seeElement('.su-card__contents');
     $I->seeElement(['css' => '.soe-link__button .button']);
     $I->click(["css" =>".soe-spotlight--cards--banner a.su-link"]);
-
   }
 
   public function testLandingPageForm(AcceptanceTester $I){
     $I->amOnPage("/spotlight");
-//    $option = $I->grabTextFrom('#edit-tid option[value="All"]');
+    $option = $I->grabTextFrom('#edit-tid option[value="All"]');
     $I->selectOption("#edit-tid" ,'Bioengineering');
-    $I->click('Filter');
+    $I->click('Apply');
+  }
+
+  /**
+   * Test that the vocabulary and terms exist.
+   */
+  public function testVocabularyTermsExists(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    $I->amOnPage("/admin/structure/taxonomy/manage/department/overview");
+    $I->canSeeNumberOfElements(".term-id", 196);
+  }
+
+  /**
+   * Test that only two of three new news nodes show up in the more news view
+   * on the node page.
+   */
+  public function testMoreNewsView(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+
+    $I->createEntity([
+      'type' => 'spotlight',
+      'title' => 'Test spotlight 1',
+    ]);
+    $I->createEntity([
+      'type' => 'spotlight',
+      'title' => 'Test spotlight 2',
+    ]);
+    $I->createEntity([
+      'type' => 'spotlight',
+      'title' => 'Test spotlight 3',
+    ]);
+
+    $I->amOnPage("/test-spotlight-2");
+    $I->see("Related spotlights");
+    $I->see("Test spotlight 1");
+    $I->see("Test spotlight 3");
   }
 }
