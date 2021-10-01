28f5038e993e8b8609c96a3eeb9caadd62f5a39c
Ian Monroe <ian@ianmonroe.com>
2021-10-01 14:14:18 -0600

    Added more magazine tests

--- tests/codeception/acceptance/Magazine/MagazineBlocksCest.php
+++ tests/codeception/acceptance/Magazine/MagazineBlocksCest.php
@@ -7,13 +7,58 @@
  */
 class MagazineBlocksCest {
 
+  use MagazineTestTrait;
   /**
    * Site managers should be able to edit custom blocks.
    */
-  public function testMagazineNavigationBlock(AcceptanceTester $I) {
+  public function testMagazineStory(AcceptanceTester $I) {
+
+    $magazineStory = $this->createMagazineNode($I, 'Magazine Test');
+
     $I->logInWithRole('site_manager');
+    /**
+     * Test the landing page exists.
+     */
     $I->amOnPage('/magazine');
     $I->canSee('Research & Ideas');
+    $I->canSee('Test Magazine Topic');
+    $I->canSee('Latest News');
+    $I->canSee('What are we working on?');
+    $I->canSee($magazineStory->getTitle());
+    /**
+     * Test the node page exists.
+     */
+    $I->amOnPage('/magazine/article/magazine-test');
+    $I->canSeeResponseCodeIs(200);
+    /**
+     * Test to make sure there's a related departments field.
+     */
+    $I->canSee('Related Departments');
+    $I->canSee('Test Department');
+    /**
+     * Test to make sure the department landing page exists.
+     */
+    $I->click('Test Department');
+    $I->seeCurrentUrlEquals('/department/test-department');
+    $I->canSee('Test Department');
+    $I->canSee($magazineStory->getTitle());
+    /**
+     * Test to make sure the magazine topic exists.
+     */
+    $I->canSee('Test Magazine Topic');
+    $I->click('Test Magazine Topic');
+    /**
+     * Test to ensure the topic landing page exists.
+     */
+    $I->seeCurrentUrlEquals('/magazine/test-magazine/topic');
+    $I->canSee('Test Magazine Topic');
+    $I->canSee($magazineStory->getTitle());
+    /**
+     * And that it links back to the correct page.
+     */
+    $I->click($magazineStory->getTitle());
+    $I->seeCurrentUrlEquals('/magazine/article/magazine-test');
+
   }
 
   public function testMagazineCurtainBlock(AcceptanceTester $I) {
@@ -22,7 +67,5 @@ class MagazineBlocksCest {
     $I->canSee('Stanford Engineering Magazine');
     $I->canSee('Explore the latest news');
   }
-
-
 
 }
