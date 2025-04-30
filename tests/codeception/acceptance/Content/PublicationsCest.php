fca917b0f15c65b644cdff1c39687f40ee780d7b
Ian Monroe <ian@ianmonroe.com>
2025-04-30 14:50:35 -0600

    tweak

--- tests/codeception/acceptance/Content/PublicationsCest.php
+++ tests/codeception/acceptance/Content/PublicationsCest.php
@@ -29,6 +29,25 @@ class PublicationsCest {
   public function __construct() {
     $this->faker = Factory::create();
   }
+  /**
+   * clear the trash directory before each test.
+   * @param AcceptanceTester $I
+   * @return void
+   */
+  public function _before(AcceptanceTester $I)
+  {
+    $trashDir = codecept_root_dir() . 'web/sites/default/files/php/trash';
+    if (is_dir($trashDir)) {
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($trashDir, RecursiveDirectoryIterator::SKIP_DOTS),
+            RecursiveIteratorIterator::CHILD_FIRST
+        );
+        foreach ($files as $fileinfo) {
+            $todo = ($fileinfo->isDir() ? 'rmdir' : 'unlink');
+            $todo($fileinfo->getRealPath());
+        }
+    }
+  }
 
   /**
    * Create a book citation
