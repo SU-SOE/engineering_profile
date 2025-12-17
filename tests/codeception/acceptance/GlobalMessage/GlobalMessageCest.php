41553133c1bee75a237d378fefcccf04deb732cc
Ian Monroe <ian@ianmonroe.com>
2025-12-17 10:31:52 -0700

    test

--- tests/codeception/acceptance/GlobalMessage/GlobalMessageCest.php
+++ tests/codeception/acceptance/GlobalMessage/GlobalMessageCest.php
@@ -41,7 +41,7 @@ class GlobalMessageCest {
   /**
    * Test the Form Settings.
    */
-  public function testFormSettings(AcceptanceTester $I) {
+  private function testFormSettings(AcceptanceTester $I) {
     $I->logInWithRole('site_manager');
     $I->amOnPage('/admin/config/system/global-message');
     $I->checkOption('#edit-su-global-msg-enabled-value');
