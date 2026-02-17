26294a3fd68b3eed6fe6d8e2bd7ed204abd01995
Jen Breese-Kauth <jbreese@stanford.edu>
2026-02-17 10:00:22 -0800

    SOEOPS-834: adding new icons and removing social media ones on news page

--- themes/engineering/dist/js/engineering.script.js
+++ themes/engineering/dist/js/engineering.script.js
@@ -9,10 +9,7 @@
     attach: function attach(context, settings) {
       $('.news-social-media', context).empty();
       $('.news-social-media', context).prepend('<div class="widget-wrapper-print"><a href="/' + settings.path.currentPath + '/printable/print" class="share-print su-news-header__social-print"><i class="fas fa-printer" aria-hidden="true"></i><span>' + Drupal.t('Print Article') + '</span></a></div>');
-      $('.news-social-media', context).prepend('<div class="widget-wrapper-forward"><a href="" class="share-forward su-news-header__social-forward"><i class="fas fa-envelope" aria-hidden="true"></i><span>' + Drupal.t('Forward Email') + '</span></a></div>');
-      $('.news-social-media', context).prepend('<div class="widget-wrapper-linkedin"><a href="" class="share-linkedin su-news-header__social-linkedin"><i aria-hidden="true"></i><span>' + Drupal.t('Stanford LinkedIn') + '</span></a></div>');
-      $('.news-social-media', context).prepend('<div class="widget-wrapper-twitter"><a href="" class="share-twitter su-news-header__social-twitter"><i aria-hidden="true"></i><span>' + Drupal.t('Stanford Twitter') + '</span></a></div>');
-      $('.news-social-media', context).prepend('<div class="widget-wrapper-fb"><a href="" class="share-fb su-news-header__social-facebook"><i aria-hidden="true"></i><span>' + Drupal.t('Stanford Facebook') + '</span></a></div>');
+      $('.news-social-media', context).prepend('<div class="widget-wrapper-copylink"><a href="" class="share-forward su-news-header__copylink"><img src="dist/assets/copy-link.png" alt="Copy Link"/>' + Drupal.t('Forward Email') + '</span></a></div>');
 
       // Get the current URL.
       var pathname = window.location;
